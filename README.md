ff3319031785d8e3379299387494158fde610006
mverch67 <manuel.verch@gmx.de>
2025-08-25 14:52:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rotary Encoder
+Arduino/platformio library for using a rotary up/down encoder with push button
+
+Source: https://github.com/CarlosSiles67/Rotary (original from Ben Buxton GPLv3)
+
+### Minor Changes:
+ - Arduino / platformio compatibility
+ - flexible choice of algorithms via compiler macros
+ - extensible C++ interface
---
+++ src/RotaryEncoder.h
@@ -0,0 +1,52 @@
+/*
+ * RotaryEncoder encoder library for Arduino.
+ */
+
+#pragma once
+
+#include "stdint.h"
+
+// Enable this to emit codes twice per step.
+//#define HALF_STEP
+
+// Enable weak pullups
+//#define ENABLE_PULLUPS
+
+
+class RotaryEncoder
+{
+  public:
+    enum ButtonState {
+      BUTTON_RESET = 0x00,
+      BUTTON_PRESSED = 0x01,
+      BUTTON_RELEASED = 0x10,
+      BUTTON_PRESSED_RELEASED = 0x11
+    };
+
+    enum Direction {
+      DIRECTION_NONE = 0x00,
+      DIRECTION_CW = 0x10,
+      DIRECTION_CCW = 0x20
+    };
+
+    RotaryEncoder(uint8_t pin1, uint8_t pin2);
+    RotaryEncoder(uint8_t pin1, uint8_t pin2, uint8_t buttonPin);
+
+    // read and process pins
+    Direction process();
+
+    bool buttonPressedReleased(uint16_t debounce_delay = 50);
+    bool buttonPressedHeld(uint16_t delay_millis = 500);
+    uint8_t readButton();
+    void resetButton();
+
+  private:
+  	void init(uint8_t pin1, uint8_t pin2);
+    uint8_t state;
+    uint8_t pin1;
+    uint8_t pin2;
+    uint8_t buttonPin;
+    uint8_t buttonState;
+    uint32_t buttonTimer;
+};
+
